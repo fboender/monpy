b2bbb0324c0288bc24492c39bee13678bbe649a9
Ferry Boender <ferry.boender@gmail.com>
2026-05-04 07:21:12 +0200

    Ignore processes that disppeared

--- collectors/processes.py
+++ collectors/processes.py
@@ -17,45 +17,49 @@ def processes():
             "environ": {},
         }
 
-        with open(os.path.join(fullpath, "cmdline"), "rb") as fh:
-            cmd = fh.read().split(b"\0")
-            cmd = [c.decode() for c in cmd if c]
-            process["cmdline"] = " ".join(cmd)
+        try:
+            with open(os.path.join(fullpath, "cmdline"), "rb") as fh:
+                cmd = fh.read().split(b"\0")
+                cmd = [c.decode() for c in cmd if c]
+                process["cmdline"] = " ".join(cmd)
 
-            try:
-                process["cwd"] = os.readlink(os.path.join(fullpath, "cwd"))
-            except FileNotFoundError:
-                pass
+                try:
+                    process["cwd"] = os.readlink(os.path.join(fullpath, "cwd"))
+                except FileNotFoundError:
+                    pass
 
-            try:
-                process["exe"] = os.readlink(os.path.join(fullpath, "exe"))
-            except FileNotFoundError:
-                pass
+                try:
+                    process["exe"] = os.readlink(os.path.join(fullpath, "exe"))
+                except FileNotFoundError:
+                    pass
 
-            try:
-                with open(os.path.join(fullpath, "environ"), "rb") as fh:
-                    for item in fh.read().split(b"\0"):
-                        try:
-                            key, value = item.decode().split("=", 1)
-                            process["environ"][key] = value.strip()
-                        except ValueError:
-                            # Some processes have a really weird environ
-                            pass
-            except ProcessLookupError:
-                pass
+                try:
+                    with open(os.path.join(fullpath, "environ"), "rb") as fh:
+                        for item in fh.read().split(b"\0"):
+                            try:
+                                key, value = item.decode().split("=", 1)
+                                process["environ"][key] = value.strip()
+                            except ValueError:
+                                # Some processes have a really weird environ
+                                pass
+                except ProcessLookupError:
+                    pass
 
-            with open(os.path.join(fullpath, "status"), "r") as fh:
-                for line in fh:
-                    key, value = line.split(":", 1)
-                    value = value.strip()
+                with open(os.path.join(fullpath, "status"), "r") as fh:
+                    for line in fh:
+                        key, value = line.split(":", 1)
+                        value = value.strip()
 
-                    if "\t" in value:
-                        value = value.split("\t")
-                    elif value.endswith("kB"):
-                        value = kb_to_bytes(value)
-                    elif value.isdigit():
-                        value = int(value)
+                        if "\t" in value:
+                            value = value.split("\t")
+                        elif value.endswith("kB"):
+                            value = kb_to_bytes(value)
+                        elif value.isdigit():
+                            value = int(value)
 
-                    process[key.lower()] = value
+                        process[key.lower()] = value
 
-        yield process
+            yield process
+        except FileNotFoundError:
+            # Process disappeared, ignore it
+            continue
