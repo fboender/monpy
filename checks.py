294d18513d5da8032f68f8e5d4c553441034b66c
Ferry Boender <ferry.boender@gmail.com>
2026-05-09 07:40:14 +0200

    Local checks

--- checks.py
+++ checks.py
@@ -397,17 +397,9 @@ def git_repo_status():
                 ident=path
             )
 
-@monpy.check(daily, daily)
-def git_repo_status_notes():
-    """
-    Check uncommited changes in Notes dir
-    """
-    path = "/home/fboender/Notes"
-    repo = collectors.git_repo(path, fetch=False)
-    if repo["has_changes"] > 0:
-        monpy.alert(
-            f"Repo '{repo['path']}' has uncommited changes",
-            ident=path
-        )
+if os.path.exists("checks_local.py"):
+    # Local only checks
+    from checks_local import register
+    register(monpy)
 
 monpy.run()
