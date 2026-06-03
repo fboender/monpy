f1b0c361949f86639ceb9790520d80fed17b227b
Ferry Boender <ferry.boender@gmail.com>
2026-06-03 09:06:00 +0200

    Script to update README automatically

---
+++ contrib/update_readme.py
@@ -0,0 +1,48 @@
+#!/bin/env texttool
+
+import subprocess
+import textwrap
+
+def get_readme_toc():
+    with open("README.md") as fh:
+        return Markdown(fh.read()).toc()
+
+def get_usage():
+    res = subprocess.run(
+        ["./checks.py", "--help"],
+        capture_output=True,
+        encoding="UTF-8",
+    )
+    return textwrap.indent(res.stdout, "    ")
+
+def get_def_check():
+    with Text("monpy/monpy.py") as doc:
+        return (
+            doc.sel(start="    def check(", end_after='"""')
+               .sel_end_fwd(end_after='"""')
+               .extract()
+        )
+
+readme_toc = get_readme_toc()
+usage = get_usage()
+def_check = get_def_check()
+
+with Text("README.md") as doc:
+    # Replace existing Table of Contents with up-to-date one
+    doc \
+        .sel(start_after="<!-- TOC -->\n", end="\n<!-- EOTOC -->")\
+        .replace(readme_toc)
+
+    # Replace Usage with up-to-date one
+    doc \
+        .sel(start_after="# Usage\n\n", end="\ncronjob:")\
+        .replace(usage)
+
+    # Replace `check()` definition with up-to-date one
+    doc \
+        .sel(start="    def check(", end_after='"""') \
+        .sel_end_fwd(end_after='"""') \
+        .replace(def_check)
+
+    doc.save()
+
